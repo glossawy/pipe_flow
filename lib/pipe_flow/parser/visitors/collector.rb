c2786920169517ad85d0888f9ed72a905c7e8c5b
Matthew Crocco <matthewcrocco@gmail.com>
2018-11-20 03:43:28 -0500

    Simplify literal collection
    This was never really working as one may expect for procs since Literals can only be a source and not a destination

--- lib/pipe_flow/parser/visitors/collector.rb
+++ lib/pipe_flow/parser/visitors/collector.rb
@@ -18,9 +18,7 @@ module PipeFlow
 
         def visit_PipeFlow_Parser_AST_Literal(literal)
           value = literal.value
-          value = ->(_input) { literal.value } unless value.is_a?(Proc)
-
-          collected << value
+          collected << ->(_input) { value }
         end
 
         def visit_PipeFlow_Parser_AST_MethodCall(method_call)
