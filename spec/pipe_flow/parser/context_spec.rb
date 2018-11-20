48af450f544c02897437d673814dd0781f018232
Matthew Crocco <matthewcrocco@gmail.com>
2018-11-19 21:21:23 -0500

    Add testing for Parser::Context

--- spec/pipe_flow/parser/context_spec.rb
+++ spec/pipe_flow/parser/context_spec.rb
@@ -1,3 +1,58 @@
 RSpec.describe PipeFlow::Parser::Context do
-  # TODO
+  let(:instance) { described_class.new(test_env) }
+  subject { instance }
+
+  describe '#parse!' do
+    class ContextTester
+      def trim(a)
+        a.strip
+      end
+
+      def intercept(x, &block)
+        block.call(x)
+        x
+      end
+    end
+
+    let(:test_env) { ContextTester.new.instance_eval { binding } }
+    let(:test_block) { proc {} }
+
+    subject { super().parse!(&test_block) }
+
+    it 'returns nil if the block does nothing' do
+      is_expected.to be_nil
+    end
+
+    context 'given a simple block with just a hole' do
+      let(:test_block) { proc { input } }
+
+      it 'returns an AST' do
+        is_expected.to be_an_ast_node
+      end
+
+      it 'returns just a hole' do
+        is_expected.to be_an_ast_hole
+      end
+    end
+
+    context 'given a simple block with just an input value' do
+      let(:input_value) { double('Input') }
+      let(:test_block) { inp = input_value; proc { input(inp) } }
+
+      it 'returns a literal value' do
+        is_expected.to be_a_literal(input_value)
+      end
+    end
+
+    context 'given a simple pipeline' do
+      let(:test_block) { proc { input >> trim >> intercept  } }
+
+      it 'returns the correct AST' do
+        is_expected.to be_pipeline_with(
+          a_pipeline_with(an_ast_hole, an_ast_methodcall),
+          an_ast_methodcall
+        )
+      end
+    end
+  end
 end
