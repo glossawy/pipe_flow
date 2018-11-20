62b4807e7ef80fb3b8112b40fdcabd76d269556e
Matthew Crocco <matthewcrocco@gmail.com>
2018-11-19 21:52:01 -0500

    Allow nodes to be marked with metadata on what where in the pipeline they may fall

--- lib/pipe_flow.rb
+++ lib/pipe_flow.rb
@@ -1,5 +1,6 @@
 require 'pipe_flow/version'
 
+require 'pipe_flow/core_refinements/class_attributes'
 require 'pipe_flow/core_refinements/pipe_flow_nodes'
 
 require 'pipe_flow/parser/ast/base'
--- lib/pipe_flow/parser/ast/base.rb
+++ lib/pipe_flow/parser/ast/base.rb
@@ -2,7 +2,18 @@ module PipeFlow
   module Parser
     module AST
       class Base
+        using CoreRefinements::ClassAttributes
         using CoreRefinements::PipeFlowNodes
+
+        class_attribute valid_source_node: true, valid_destination_node: true
+
+        def self.source_only_node!
+          self.valid_destination_node = false
+        end
+
+        def self.destination_only_node!
+          self.valid_source_node = false
+        end
 
         def >>(other)
           AST::Pipe.new(self, other.to_pipe_flow_node)
