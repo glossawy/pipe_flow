490d82a6f2fc1c62f86d2dc660e24dc01940ddd5
Glossawy <4606234+Glossawy@users.noreply.github.com>
2018-11-20 01:27:11 -0500

    Create collector visitor implementation
    This transforms the AST into an array of procs that can be composed
    in serial.

---
+++ spec/pipe_flow/parser/visitors/collector_spec.rb
@@ -0,0 +1,76 @@
+RSpec.describe PipeFlow::Parser::Visitors::Collector do
+  let(:instance) { described_class.new }
+
+  context '#visit' do
+    subject { instance.collect(input) }
+    let(:collected) { instance.collected }
+
+    context 'for a AST::Hole' do
+      let(:input) { PipeFlow::Parser::AST::Hole.instance }
+      before { subject }
+
+      it 'collects a single result' do
+        expect(collected.size).to eq(1)
+      end
+
+      it 'results in a proc' do
+        expect(collected.first).to be_a(Proc)
+      end
+    end
+
+    context 'for a AST::Literal' do
+      let(:input) { PipeFlow::Parser::AST::Literal.new(double('Literal Value')) }
+      before { subject }
+
+      it 'collects a single result' do
+        expect(collected.size).to eq(1)
+      end
+
+      it 'results in a proc' do
+        expect(collected.first).to be_a(Proc)
+      end
+    end
+
+    context 'for a AST::MethodCall' do
+      let(:fake_env) { object_double(binding) }
+      let(:fake_self) { double('Receiver') }
+
+      let(:input) { PipeFlow::Parser::AST::MethodCall.new(fake_env, :test_method, []) }
+
+      before do
+        allow(fake_env).to receive(:eval).with(anything).and_return([], fake_self)
+        subject
+      end
+
+      it 'collects a single result' do
+        expect(collected.size).to eq(1)
+      end
+
+      it 'results in a proc' do
+        expect(collected.first).to be_a(Proc)
+      end
+    end
+
+    context 'for a AST::Pipe' do
+      let(:input) { PipeFlow::Parser::AST::Pipe.new(source, destination) }
+      let(:source) { PipeFlow::Parser::AST::Hole.instance }
+      let(:destination) { PipeFlow::Parser::AST::Literal.new(double('Literal Value')) }
+
+      it 'collects two results' do
+        subject
+        expect(collected.size).to eq(2)
+      end
+
+      it 'visits the source and destination nodes as a pre-order traversal', aggregate_failures: true do
+        expect(instance).to receive(:visit).with(source).and_call_original.ordered
+        expect(instance).to receive(:visit).with(destination).and_call_original.ordered
+        subject
+      end
+
+      it 'results in only procs' do
+        subject
+        expect(collected).to all(be_a Proc)
+      end
+    end
+  end
+end
