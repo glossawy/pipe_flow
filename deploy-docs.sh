b7debc2537213248a3a3f4e15768b0fcae4c2190
Matthew Crocco <matthewcrocco@gmail.com>
2018-12-02 19:43:49 -0500

    Move to build matrix on multiple rubies, use separate docs branch
    We will now test against rubies for 2.2, 2.3, 2.4, and 2.5. Additionally, documentation will be automatically updated as part of pushes to master if there are any changes.

---
+++ deploy-docs.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -euo pipefail
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+mkdir -p ./docs/  # In case docs/ is ever deleted for some reason
+
+branch_is_new=true
+(
+  # Setup git in docs directory
+  cd docs
+  git init
+  git remote add origin https://${GH_TOKEN}@github.com/Matt529/pipe_flow.git > /dev/null 2>&1
+  git fetch >/dev/null
+  if git checkout gh-pages 2>/dev/null; then
+    branch_is_new=false
+  else
+    git checkout -b gh-pages
+    touch .nojekyll
+  fi
+  git fetch >/dev/null
+  git reset origin/gh-pages --hard >/dev/null
+)
+
+# Install and run yard
+gem list -i yard >/dev/null 2>&1 || gem install yard
+yardoc
+
+cd docs
+
+if ${branch_is_new} || git status --porcelain; then
+  # Commit and push updated documentation
+  git add ./
+  git commit -m "Build ${TRAVIS_BUILD_NUMBER}: Documentation Update"
+  git push --quiet --set-upstream origin gh-pages
+fi
